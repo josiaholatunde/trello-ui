80dd62435036a8db51a2775244bd7c0fa6c6788f
Ogunboyejo Olatunde <tunde.ogunboyejo@xputertechnologies.com>
2019-02-14 12:55:40 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { CtaComponent } from './components/cta/cta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    GalleryComponent,
+    OverviewComponent,
+    DetailComponent,
+    CtaComponent
   ],
   imports: [
     BrowserModule
